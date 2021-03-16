7d591044d0a0e696073436685fa1b92651a413d3
info-arnav <info@arnavgupta.net>
2021-03-17 01:23:48 +0530

    initialig

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import { signIn, signOut, useSession, Provider } from "next-auth/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navigation from "../components/Navigation";
+import "../styles/globals.css";
+import "../styles/globals.scss";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Provider session={pageProps.session}>
+        <Navigation></Navigation>
+        <main>
+          <Component {...pageProps} />
+        </main>
+      </Provider>
+    </>
+  );
 }
-
-export default MyApp
