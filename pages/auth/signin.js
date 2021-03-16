f217b3df9d4c3367403343bac8681b6e14b67d57
info-arnav <info@arnavgupta.net>
2021-03-17 02:15:51 +0530

    loggedIn

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,15 +1,20 @@
-import { providers, signIn } from "next-auth/client";
+import { providers, signIn, useSession } from "next-auth/client";
+import { useRouter } from "next/router";
 
 export default function SignIn({ providers }) {
+  const [session, loading] = useSession();
+  const router = useRouter();
   return (
     <>
-      {Object.values(providers).map((provider) => (
-        <div key={provider.name}>
-          <button onClick={() => signIn(provider.id)}>
-            Sign in with {provider.name}
-          </button>
-        </div>
-      ))}
+      {!session &&
+        Object.values(providers).map((provider) => (
+          <div key={provider.name}>
+            <button onClick={() => signIn(provider.id)}>
+              Sign in with {provider.name}
+            </button>
+          </div>
+        ))}
+      {session && router.push("/dashboard")}
     </>
   );
 }
--- pages/auth/verify-request.js
+++ pages/auth/verify-request.js
@@ -1,8 +1,10 @@
 import { signIn, signOut, useSession } from "next-auth/client";
+import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 export default function Page() {
   const [session, loading] = useSession();
+  const router = useRouter();
   return (
     <>
       {!session && (
@@ -11,12 +13,7 @@ export default function Page() {
           <button onClick={() => signIn()}>Sign in</button>
         </>
       )}
-      {session && (
-        <>
-          Signed in as {session.user.email} <br />
-          <button onClick={() => signOut()}>Sign out</button>
-        </>
-      )}
+      {session && router.push("dashboard")}
     </>
   );
 }
