4fac4a705420cf8ec09fcc2645f87c989bba966e
info-arnav <info@arnavgupta.net>
2021-03-18 02:20:21 +0530

    locale

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  i18n: {
+    locales: ["en-US"],
+    defaultLocale: "en-US",
+    domains: [
+      {
+        domain: "www.arnavgupta.net",
+        defaultLocale: "en-US",
+      },
+    ],
+  },
+};
